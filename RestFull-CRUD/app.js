bc1fc9f16e45bf1c9c38f10c96c86f9d6c1f929c
pashacaclaudia <pashaca.claudia.carolina@iisgalvanimi.edu.it>
2020-04-01 19:05:43 +0000

    mean app completa

--- RestFull-CRUD/app.js
+++ RestFull-CRUD/app.js
@@ -7,6 +7,7 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var meanRouter = require('./routes/mean');
 
 var app = express();
 app.use(cors());
@@ -21,8 +22,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', meanRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
